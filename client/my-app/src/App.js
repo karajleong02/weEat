894a5e506ba9ecf30aca75f948ede8f2b37bec30
karaleong <kleong8@gatech.edu>
2022-10-22 22:45:20 -0400

    login page

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -12,7 +12,6 @@ function App() {
 
   return (
     <div>
-
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
--- client/my-app/src/pages/Suggestions.jsx
+++ client/my-app/src/pages/Suggestions.jsx
@@ -10,8 +10,12 @@ function Suggestions() {
             <Grid container spacing={8} align="center">
                 <Grid item xs={3}>
                     <Restaurant
-                        name=""
-                        
+                        name="Dine"
+                        distance="1.5"
+                        price="$$"
+                        cuisine="American"
+                        url="https://www.mcdonalds.com/us/en-us.html"
+                        imgURL="https://img.freepik.com/free-photo/hamburger-with-beef-meat-burger-fresh-vegetables-dark-surface_2829-5883.jpg?w=2000&t=st=1666490821~exp=1666491421~hmac=28b0d17814f2de389e886ac794e9109be9e602b13a1a19c65d087843a3403478"
                     ></Restaurant>
                 </Grid>
                 <Grid item xs={3}>
